7920075bc16c9ae1e4b9c771675240544232af51
trippian1 <167805959+trippian1@users.noreply.github.com>
2024-04-26 18:21:18 -0400

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact Us</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="contact.css"> 
+
+<!--
+			Author:		    Ian Tripp
+			Class:			CSCI 1210
+			Date:			04/26/2024
+			Assignment:		Phase 3
+			Purpose:		Contact Us Page
+--> 
+</head>
+<body>
+	<div class="wrapper">
+		<?php include 'header.php';?>
+		<?php include 'nav.php';?>
+		
+		<section>
+			<h1>Contact Us!</h1>
+			<article>
+				<p style=color: rgba(0,0,0,0)>
+					<ul>
+						<li><a href="https://www.facebook.com/ukirketsu/" class= "class2">Facebook</a>&nbsp|&nbsp</li>
+						<li><a href="https://www.instagram.com/ukirketsu/?hl=en" class= "class2">Instagram</a>&nbsp|&nbsp</li>
+						<li><a href="/map_to_ukirk.jpg" class= "class2">Map to UKirk</a>&nbsp|&nbsp</li>
+						<li>Email: ukirketsu@gmail.com &nbsp|&nbsp</li>
+						<li>Address: 1412 College Heights Rd, Johnson City, TN 37604 &nbsp|&nbsp</li>
+						<li>Phone Number: (423) 926-2522</li>
+					</ul>
+				</p>
+			</article>
+		</section>
+		<?php include 'footer.php';?>
+	</div>
+	<?php include 'w3c.php';?>
+</body>
+
+</html>        
